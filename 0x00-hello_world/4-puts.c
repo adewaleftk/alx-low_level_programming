622f5939b2eee274da2e9f1d4eefd1fdc7642ca0
Adewale Fatoki <adewaleftk@gmail.com>
2022-10-13 06:13:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - main function
- *   * Return: int
- *    */
+ *   main - main function
+ *   Return: int
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
