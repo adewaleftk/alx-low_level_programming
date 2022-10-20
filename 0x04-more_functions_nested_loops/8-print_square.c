6b3b0a177cbd9ac7c6baae112a2fd7cec555686b
Adewale Fatoki <adewaleftk@gmail.com>
2022-10-20 21:39:17 +0100

    u

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print a square to the screen
+ * @size: the size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
