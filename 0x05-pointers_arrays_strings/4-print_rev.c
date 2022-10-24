49714509b8cd1686d0645a86bb60b04b3a1b2f3d
Adewale Fatoki <adewaleftk@gmail.com>
2022-10-24 16:36:52 +0100

    printrev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: the string to print in reverse
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
