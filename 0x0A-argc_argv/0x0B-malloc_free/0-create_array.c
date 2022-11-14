ebf404bd2fc90a8dd9be9b7e6f4632b2156c09cf
Adewale Fatoki <adewaleftk@gmail.com>
2022-11-14 07:36:39 +0100

    create array

---
+++ 0x0A-argc_argv/0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>i
+#include "main.h"
+
+/**
+ * create_array - create an array of chars, and initialize it with c
+ * @size: the size of the array
+ * @c: the char to initialize the array with
+ * Return: pointer to the array, or NULL if unsuccessful
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(size * sizeof(c));
+
+	if (arr == NULL || size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
