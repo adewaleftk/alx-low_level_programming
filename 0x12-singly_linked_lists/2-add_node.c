896e9ef5cf5f78ca7cbb4ef846916b528b60841d
Adewale Fatoki <adewaleftk@gmail.com>
2022-11-25 02:26:08 +0100

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.c"
+
+/**
+ * add_node - add a node to the head of a list
+ *
+ * @head: list head
+ * @str: string value
+ * Return: pointer to new head node
+ *
+ * list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int len = 0;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	while (*(str + len))
+		len++;
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}/
