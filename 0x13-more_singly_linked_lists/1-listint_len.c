576f8b2117337fcbc9ee6a638252e9c007f635ae
Adewale Fatoki <adewaleftk@gmail.com>
2022-11-29 13:49:43 +0100

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * listint_len - find number of elements in a listint_t
+ *
+ * @h: listint_t
+ * Return: number of elements in h
+ */
 
 size_t listint_len(const listint_t *h);
 {
